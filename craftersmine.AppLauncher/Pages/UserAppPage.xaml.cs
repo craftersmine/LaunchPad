bea8ff3a9d2590c4c496244a94d4521a2bfb8ac9
craftersmine <rodion.morozov.01@gmail.com>
2022-07-06 14:02:34 +0700

    Added Cover background image

--- craftersmine.AppLauncher/Pages/UserAppPage.xaml.cs
+++ craftersmine.AppLauncher/Pages/UserAppPage.xaml.cs
@@ -16,6 +16,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Controls;
 using SplitButton = Microsoft.UI.Xaml.Controls.SplitButton;
 using SplitButtonClickEventArgs = Microsoft.UI.Xaml.Controls.SplitButtonClickEventArgs;
 
